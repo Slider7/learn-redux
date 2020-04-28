44a02c349a44b37beef8e1cba13f604b2aef3d86
Kemalkhan Shlembayev <kemalsh771@gmail.com>
2020-04-28 10:54:42 +0600

    final commit

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectSong } from '../actions'; //action creator
 
 class SongList extends Component {
+  renderList() {
+    return this.props.songs.map(song => {
+      return (
+        <div className="item" key={song.title}>
+          <div className="right floated content">
+            <button
+              className="ui button primary"
+              onClick={() => this.props.selectSong(song)}
+            >
+              Select
+            </button>
+          </div>
+          <div className="content">{song.title}</div>
+        </div>
+      );
+    });
+  }
   render() {
-    console.log(this.props);
-    return <div>SongList</div>;
+    return <div className="ui divided list">{this.renderList()}</div>;
   }
 }
 
@@ -12,8 +29,11 @@ const mapStateToProps = state => {
   return { songs: state.songs };
 };
 
-export default connect(mapStateToProps)(SongList);
+export default connect(mapStateToProps, { selectSong })(SongList);
 
 //Всегда импортируем connect, и всегда его экспортируем с аргументами:
 //1-арг. - mapStateToProps(всегда передаем ей стейт и возвращаем пропс)
 //2-арг. - наш компонент
+
+// {selectSong: selectSong} - этот объект передаем в connect, вместе с mapStateToProps
+// таким образом action creator связывается с компонентом - через пропс
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,10 @@ import { combineReducers } from 'redux';
 
 const songsReducer = () => {
   return [
-    { title: 'No crubs', durations: '4:05' },
-    { title: 'No Macarena', durations: '2:30' },
-    { title: 'All Stars', durations: '3:15' },
-    { title: 'I want it my way', durations: '1:45' }
+    { title: 'No crubs', duration: '4:05' },
+    { title: 'No Macarena', duration: '2:30' },
+    { title: 'All Stars', duration: '3:15' },
+    { title: 'I want it my way', duration: '1:45' }
   ];
 };
 
